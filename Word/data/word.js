9afabf38b9415bedab524a2b2862df8e96ecfd6e
harishkumarrao <harishk551@gmail.com>
2019-04-09 02:23:06 +0530

    code changes

--- Word/data/word.js
+++ Word/data/word.js
@@ -1,16 +1,86 @@
-	var data = `ability
-	world
-worry
-would
-write
-writer
-wrong
-yard
-yeah
-year
-yes
-yet
-you
-young
-your
-yourself`;
+var data = `
+abandon
+abandoned
+ability
+able
+about
+above
+abroad
+absence
+absent
+absolute
+absolutely
+absorb
+abuse
+abuse
+academic
+accent
+accept
+acceptable
+access
+accident
+accidental
+accidentally
+accommodation
+accompany
+according to
+account
+account for
+accurate
+accurately
+accuse
+achieve
+achievement
+acid
+acknowledge
+a couple
+acquire
+across
+act
+action
+active
+actively
+activity
+actor
+actress
+actual
+actually
+ad
+adapt
+add
+addition
+additional
+add on
+address
+add up
+add up to
+adequate
+adequately
+adjust
+admiration
+admire
+admit
+adopt
+adult
+advance
+advanced
+advantage
+adventure
+advert
+advertise
+advertisement
+advertising
+advice
+advise
+affair
+affect
+affection
+afford
+afraid
+after
+afternoon
+afterwards
+again
+against
+age
+aged`;
